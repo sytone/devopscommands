ba0dea2d9d6bfd7ec787b6e489ebd175e5c707bc
Sytone <github@sytone.com>
2025-11-21 15:52:01 -0800

    Update vswhere command to include prerelease option

--- Private/Get-VisualStudioDetail.ps1
+++ Private/Get-VisualStudioDetail.ps1
@@ -9,7 +9,10 @@ function Get-VisualStudioDetail {
 
     process {
         # Get the Visual Studio 2022 shell path based on the version (Preview or Enterprise).
-        $vsLocationDetails = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere" -format json -version $MajorVersion | ConvertFrom-Json
+        $vsLocationDetails = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere" -format json -prerelease | ConvertFrom-Json
+
+        # Find the correct installation based on the parameters. Start with major version filtering.
+        $vsLocationDetails = $vsLocationDetails | Where-Object { $_.installationVersion -like "$MajorVersion.*" }
 
         if ($UsePreview) {
             $vsLocation = $vsLocationDetails | Where-Object { $_.channelId -like "*Preview" }
@@ -17,7 +20,7 @@ function Get-VisualStudioDetail {
             $vsLocation = $vsLocationDetails | Where-Object { $_.channelId -notlike "*Preview" }
         }
 
-        return ($vsLocation.installationPath + "\Common7\Tools\Launch-VsDevShell.ps1"), ($vsLocation.displayName)
+        return ("$($vsLocation.installationPath)\Common7\Tools\Launch-VsDevShell.ps1"), ($vsLocation.displayName)
     }
 
     end {
