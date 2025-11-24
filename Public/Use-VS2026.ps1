4fc82d631ba72ff52525ad7b3b467ef830be7bb7
Sytone <github@sytone.com>
2025-11-24 09:21:59 -0800

    Add Use-VS2026 function to load build environment

---
+++ Public/Use-VS2026.ps1
@@ -0,0 +1,46 @@
+function Use-VS2022 {
+    <#
+    .SYNOPSIS
+        Load the build environment for Visual Studio 2026
+    .DESCRIPTION
+        Load the build environment for Visual Studio 2026 using the Visual Studio 2026 development settings.
+    .EXAMPLE
+        Use-VS2022
+    #>
+    param (
+        [switch] $UsePreview
+    )
+    begin {
+        Write-HeadingBlock (Get-ModuleHeaderInfo)
+        $completedSuccessfully = $false
+
+        # Clean up environment variables to ensure no newlines or excessive length as they cause issues in the shell.
+        Get-ChildItem env: | ForEach-Object {
+            Repair-EnvironmentVariable -EnvironmentVariableName $_.Name
+        }
+
+        $shellPath, $version = Get-VisualStudioDetail -MajorVersion 18 -UsePreview:$UsePreview
+    }
+
+    process {
+
+        if (-not (Test-Path (Split-Path $shellPath -Parent))) {
+            $completedSuccessfully = $false
+        } else {
+
+            Push-Location (Split-Path $shellPath -Parent)
+            Write-Information "------------------------------------------------------------"
+            Write-Information " * Setting up environment..."
+            Write-Information "   - $version Command Prompt processing."
+            & $shellPath
+            Pop-Location
+            Write-Information "   - $version Command Prompt variables set."
+            $completedSuccessfully = $true
+        }
+    }
+
+    end {
+        return $completedSuccessfully
+    }
+}
+
